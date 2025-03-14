8899da6689243741bda9b1816dfd359acce7e4d8
YuniqCoding <totquf52@naver.com>
2025-03-14 17:15:42 +0900

    250314commit

---
+++ 02_VUE/chapter02/02-11.js
@@ -0,0 +1,14 @@
+let obj = { result: 0 };
+obj.add = function (x, y) {
+  this.result = x + y;
+};
+
+let add2 = obj.add;
+// 두개의 함수가 같다고 출력됨
+console.log(add2 === obj.add);
+
+// 호출하는 곳이 전역이기 때문에 result가 전역 범위에 생성
+// window.result = 7
+add2(3, 4);
+console.log(obj);
+console.log(result);
---
+++ 02_VUE/chapter02/02-14.js
@@ -0,0 +1,13 @@
+let obj = { result: 0 };
+obj.add = function (x, y) {
+  function inner() {
+    this.result = x + y;
+  }
+  // bind를 통해서 this 값을 지정한다
+  // add의 this를 inner의 this에 지정
+  inner = inner.bind(this);
+  inner();
+};
+
+obj.add(3, 4);
+console.log(obj);
